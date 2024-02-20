f499ab3d09ce59488f3235a63a6b476a09181198
Razzi Abuissa <razzi@abuissa.net>
2024-02-20 15:11:08 -0600

    oops remove junk

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createServer } = require('node:http');
 
 const app = express();
 const server = createServer(app);
-fs
+
 app.get('/', (_, res) => {
   res.write('<h1>Hello world</h1>');
   setInterval(() => {res.write('<h1>Hello world2</h1>')}, 1000);
@@ -12,5 +12,3 @@ app.get('/', (_, res) => {
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
 });
-
-alert("hi")
