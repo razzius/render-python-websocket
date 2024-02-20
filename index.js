1d47c4f4f8590559abcaada7dc3f534eaa4f6ee9
Razzi Abuissa <razzi@abuissa.net>
2024-02-20 15:17:14 -0600

    add index.html

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const { createServer } = require('node:http');
+const { readFileSync } = require('fs')
 
 const app = express();
 const server = createServer(app);
 
 app.get('/', (_, res) => {
-  res.write('<h1>Hello world</h1>');
-  setInterval(() => {res.write('<h1>Hello world2</h1>')}, 1000);
+  const indexData = readFileSync('index.html')
+  res.send(indexData.toString());
+//   setInterval(() => {res.write('<h1>Hello world2</h1>')}, 1000);
 });
 
 server.listen(3000, () => {
